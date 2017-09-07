96bde99ee16e7f5b4fcd355a2a46c69e386f4964
Adnan Mirza <adnan_pay@hotmail.com>
2017-09-07 20:23:11 +0100

    Implemented Move and passed first set of tests

--- PlutoRover/Rover.cs
+++ PlutoRover/Rover.cs
@@ -14,13 +14,61 @@ namespace PlutoRover
 
         public Rover()
         {
-            throw new NotImplementedException();
+            X = 0;
+            Y = 0;
+            Direction = "N";
         }
 
         //Move rover according to input
-        public void Move(string input, string currentDirection)
+        public void Move(string input, string direction)
         {
-            throw new NotImplementedException();
+            switch (direction)
+            {
+                case "N":
+                    switch (input)
+                    {
+                        case "F":
+                            Y++;
+                            break;
+                        case "B":
+                            Y--;
+                            break;
+                    }
+                    break;
+                case "E":
+                    switch (input)
+                    {
+                        case "F":
+                            X++;
+                            break;
+                        case "B":
+                            X--;
+                            break;
+                    }
+                    break;
+                case "S":
+                    switch (input)
+                    {
+                        case "F":
+                            Y--;
+                            break;
+                        case "B":
+                            Y++;
+                            break;
+                    }
+                    break;
+                case "W":
+                    switch (input)
+                    {
+                        case "F":
+                            X--;
+                            break;
+                        case "B":
+                            X++;
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
