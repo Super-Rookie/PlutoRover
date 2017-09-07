93ece818ef09152e1eca0d978a50f597645c58e4
Adnan Mirza <adnan_pay@hotmail.com>
2017-09-07 20:14:35 +0100

    Add Rover class and first set of tests for command F & B

---
+++ PlutoRover/Rover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoRover
+{
+    public class Rover
+    {
+        public int X;
+        public int Y;
+        public string Direction;
+
+        public Rover()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Move rover according to input
+        public void Move(string input, string currentDirection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RoverTest/RoverTesting.cs
+++ RoverTest/RoverTesting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlutoRover;
 
 namespace RoverTest
 {
@@ -7,8 +8,149 @@ namespace RoverTest
     public class RoverTesting
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InitialisingRover()
         {
+            var rover = new Rover();
+        }
+
+        [TestMethod]
+        public void MovingFWhileFacingNOnlyIncrementsYOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+
+            var y = rover.Y;
+            var d = rover.Direction;
+
+            rover.Move("F", "N");
+
+            var newY = rover.Y;
+            var newD = rover.Direction;
+
+            var distance = newY - y;
+
+            Assert.AreEqual(distance, 1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingBWhileFacingNOnlyDecrementsYOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+
+            var y = rover.Y;
+            var d = rover.Direction;
+            rover.Move("B", "N");
+
+            var newY = rover.Y;
+            var newD = rover.Direction;
+
+            var distance = newY - y;
+
+            Assert.AreEqual(distance, -1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingFWhileFacingEOnlyIncrementsXOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+            
+            var x = rover.X;
+            var d = rover.Direction;
+
+            rover.Move("F", "E");
+
+            var newX = rover.X;
+            var newD = rover.Direction;
+
+            var distance = newX - x;
+            Assert.AreEqual(distance, 1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingBWhileFacingEOnlyDecrementsXOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+
+            var x = rover.X;
+            var d = rover.Direction;
+            rover.Move("B", "E");
+
+            var newX = rover.X;
+            var newD = rover.Direction;
+
+            var distance = newX - x;
+            Assert.AreEqual(distance, -1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingFWhileFacingSOnlyDecrementsYOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+            
+            var y = rover.Y;
+            var d = rover.Direction;
+            rover.Move("F", "S");
+
+            var newY = rover.Y;
+            var newD = rover.Direction;
+
+            var distance = newY - y;
+            Assert.AreEqual(distance, -1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingBWhileFacingSOnlyIncrementsYOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+            var y = rover.Y;
+            var d = rover.Direction;
+            rover.Move("B", "S");
+
+            var newY = rover.Y;
+            var newD = rover.Direction;
+
+            var distance = newY - y;
+
+            Assert.AreEqual(distance, 1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingFWhileFacingWOnlyDecrementsXOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+            
+            var x = rover.X;
+            var d = rover.Direction;
+
+            rover.Move("F", "W");
+
+            var newX = rover.X;
+            var newD = rover.Direction;
+
+            var distance = newX - x;
+            Assert.AreEqual(distance, -1);
+            Assert.AreEqual(d, newD);
+        }
+
+        [TestMethod]
+        public void MovingBWhileFacingWOnlyIncrementsXOnceAndStillFaceSameDirection()
+        {
+            var rover = new Rover();
+            var x = rover.X;
+            var d = rover.Direction;
+            rover.Move("B", "W");
+
+            var newX = rover.X;
+            var newD = rover.Direction;
+
+            var distance = newX - x;
+            Assert.AreEqual(distance, 1);
+            Assert.AreEqual(d, newD);
         }
     }
 }
