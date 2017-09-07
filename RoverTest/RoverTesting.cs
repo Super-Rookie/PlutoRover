8bd1b4b0261bd0f80749faf5ef725dbc5c8bddc2
Adnan Mirza <adnan_pay@hotmail.com>
2017-09-07 20:01:21 +0100

    Added Solution

---
+++ RoverTest/RoverTesting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoverTest
+{
+    [TestClass]
+    public class RoverTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
